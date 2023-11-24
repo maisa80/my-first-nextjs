a9464e00dd50286892edb7891068d1959b81114e
Maisa Abusalem <94901967+maisa80@users.noreply.github.com>
2023-11-24 01:52:26 +0100

    change title

--- app/page.jsx
+++ app/page.jsx
@@ -11,14 +11,14 @@ export default function Home() {
         </Link>
       </div>
 
-      <h2>Company Updates</h2>
+      <h2>Blog Updates</h2>
 
       <div className="card">
-        <h3>New member of the web dev team...</h3>
+        <h3>New authors join our team...</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at quam. Dolores omnis possimus quam soluta rerum illo laborum ullam pariatur molestiae, modi beatae corrupti.</p>
       </div>
       <div className="card">
-        <h3>New website live!</h3>
+        <h3>New posts live!</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at quam. Dolores omnis possimus quam soluta rerum illo laborum ullam pariatur molestiae, modi beatae corrupti, assumenda distinctio adipisci, cupiditate minima eum vitae? Similique dicta est facilis debitis, autem temporibus quo repellat illum unde id iste veritatis eveniet, aspernatur enim quas.</p>
       </div>
     </main>
